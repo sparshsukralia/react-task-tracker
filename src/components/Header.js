6b3afbc5e185a456e8ef2a3e567f3aa65135f193
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-10-24 18:03:53 +0530

    button

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <button className="btn">Add</button>
+    </header>
+  );
+};
+
+export default Header;
