da3bb9bb22501bdf387ebc1a08af4203993eb8e4
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-10-25 17:12:46 +0530

    deleteTask

--- src/App.js
+++ src/App.js
@@ -26,13 +26,25 @@ function App() {
   ]);
 
   const deleteTask = (id) => {
-    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
